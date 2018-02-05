18a46e8e878774e918da88f2709821a56c66c660
Munna Gupta <munnaguta480@gmail.com>
2018-02-06 02:02:53 +0530

    flatlist done

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { List, ListItem, SearchBar } from 'react-native-elements';
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true,
+		};
+	}
+
+	componentDidMount() {
+		return fetch('https://randomuser.me/api/')
+			.then(response => response.json())
+			.then(responseJson => {
+				this.setState(
+					{
+						isLoading: false,
+						data: response.results,
+					},
+					function() {
+						// do something with new state
+					}
+				);
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
+
 	render() {
+		if (this.state.isLoading) {
+			return (
+				<View style={{ flex: 1, paddingTop: 20 }}>
+					<ActivityIndicator />
+				</View>
+			);
+		}
+
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
-			</View>
+			<List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+				<FlatList
+					data={this.state.data}
+					renderItem={({ item }) => (
+						<ListItem
+							roundAvatar
+							title={`${item.name.first} ${item.name.last}`}
+							subtitle={item.email}
+							avatar={{ uri: item.picture.thumbnail }}
+							containerStyle={{ borderBottomWidth: 0 }}
+						/>
+					)}
+				/>
+			</List>
 		);
 	}
 }
